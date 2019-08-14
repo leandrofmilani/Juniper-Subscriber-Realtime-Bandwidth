378e05a3fbc7dce0f4da734eb9331e47f4dd15ec
Leandro Fabris Milani <leandrofmilani@gmail.com>
2019-08-14 19:29:10 +0100

    Added DataRequired for IP field

--- forms.py
+++ forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, Length, IPAddress
 
 class UsernameForm(FlaskForm):
     username = StringField('Login PPPoE', validators=[DataRequired(),Length(min=2, max=50)])
-    ip = StringField('Juniper IP', validators=[IPAddress(ipv4=True, ipv6=False, message='Invalid IP address')])
+    ip = StringField('Juniper IP', validators=[DataRequired(),IPAddress(ipv4=True, ipv6=False, message='Invalid IP address')])
     submit = SubmitField('Connect')
